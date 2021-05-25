f33113c48fd84c05b4805853b31db83680e7edf0
yunsung <12345ggh81@gmail.com>
2021-05-25 21:02:42 +0900

    Certificatio_학교인증

---
+++ app/src/main/java/com/example/tita/SignUp_CertificatioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp_CertificatioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_certificatio)
+    }
+}
