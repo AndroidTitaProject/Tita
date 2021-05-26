7b96e04b7c4035dab850eb52a470bc9cd5f1daac
yunsung <12345ggh81@gmail.com>
2021-05-26 09:13:19 +0900

    [UPDATE]SignUpCertificatio_id_layout

--- app/src/main/java/com/example/tita/SignUpCertificatioActivity.kt
+++ app/src/main/java/com/example/tita/SignUpCertificatioActivity.kt
@@ -3,7 +3,7 @@ package com.example.tita
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignUp_CertificatioActivity : AppCompatActivity() {
+class SignUpCertificatioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_certificatio)
