55d2159e0eaa1821795aa14de6bdf337d034172b
yunsung <12345ggh81@gmail.com>
2021-05-26 12:21:54 +0900

    [UPDATE]authority_Update

--- app/src/main/java/com/example/tita/viewmodels/SignUpCertificatioViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tita.viewmodels
-
-import android.app.Application
-import com.example.tita.base.BaseViewModel
-
-class SignUpCertificatioViewModel(application: Application) : BaseViewModel(application) {
-
-}
