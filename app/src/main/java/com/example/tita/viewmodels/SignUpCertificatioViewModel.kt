c83314018fe79b715d4ab9b9bdecdc9eaf54f786
KyungMin <2004ckck@naver.com>
2021-05-26 20:34:59 +0900

    [CREATE] SignUpViewModel

---
+++ app/src/main/java/com/example/tita/viewmodels/SignUpCertificatioViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.tita.viewmodels
+
+import android.app.Application
+import com.example.tita.base.BaseViewModel
+
+class SignUpCertificatioViewModel(application: Application) : BaseViewModel(application) {
+
+}
