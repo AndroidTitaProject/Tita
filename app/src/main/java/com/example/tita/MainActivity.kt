035c299925de88a06c274e0aa867c20cd3218623
yunsung <12345ggh81@gmail.com>
2021-05-29 23:56:25 +0900

    [UPDATE] MainPage_Code Update

--- app/src/main/java/com/example/tita/MainActivity.kt
+++ app/src/main/java/com/example/tita/MainActivity.kt
@@ -1,8 +1,7 @@
-package com.example.tita.ui.activity
+package com.example.tita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tita.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
