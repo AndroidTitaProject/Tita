4d9e3cf96661f7ec87121c641c9b4e5fa872543a
KyungMin <2004ckck@naver.com>
2021-05-30 19:50:45 +0900

    [CREATE] Login ViewModel 생성

---
+++ app/src/main/java/com/example/tita/viewmodels/LoginViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.tita.viewmodels
+
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.view.View
+import androidx.core.content.ContextCompat.startActivity
+import com.example.tita.base.BaseViewModel
+import com.example.tita.ui.activity.LoginActivity
+import com.example.tita.ui.fragment.SignUptermsFragment
+
+class LoginViewModel(application: Application) : BaseViewModel(application) {
+
+
+
+}
