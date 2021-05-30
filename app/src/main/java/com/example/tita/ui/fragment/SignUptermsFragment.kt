3ccc1dac87917c5ca1a971c1bf82c0dcacc95a33
KyungMin <2004ckck@naver.com>
2021-05-30 19:50:06 +0900

    [UPDATE] dataBinding- > ViewBinding 으로 수정

--- app/src/main/java/com/example/tita/ui/fragment/SignUptermsFragment.kt
+++ app/src/main/java/com/example/tita/ui/fragment/SignUptermsFragment.kt
@@ -1,39 +1,33 @@
 package com.example.tita.ui.fragment
 
-import android.content.ContentValues.TAG
+import android.content.ContentValues
+import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tita.R
-import com.example.tita.base.BaseFragment
 import com.example.tita.databinding.FragmentSignUpBinding
-import com.example.tita.viewmodels.SignUpViewModel
-import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class SignUptermsFragment : BaseFragment<FragmentSignUpBinding, SignUpViewModel>() {
-    override val layoutResID: Int
-        get() = R.layout.fragment_sign_up
+class SignUptermsFragment : Fragment() {
 
-    override val viewModel: SignUpViewModel
-        get() = getViewModel(SignUpViewModel::class)
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
 
-    override fun getFragmentBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
+        binding.nextButton.setOnClickListener {
 
-    ): FragmentSignUpBinding {
-        return FragmentSignUpBinding.inflate(inflater, container, false)
-    }
-
-    fun intentSignUpTerms(view: View) {
-        activity?.supportFragmentManager?.beginTransaction()?.replace(
-            R.id.sign_Up_text,
-            SignUpCertificatioFragment()
-        )?.commit()
+        }
+        return binding.root
     }
 
 }
