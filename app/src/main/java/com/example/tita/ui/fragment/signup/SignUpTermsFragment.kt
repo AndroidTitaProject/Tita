bd5a533a53561a9bcb58713d1d83467156d54a53
KyungMin <2004ckck@naver.com>
2021-06-02 18:05:49 +0900

    [CREATE]  BottomNavigation의 SignUp.kt 생성

---
+++ app/src/main/java/com/example/tita/ui/fragment/signup/SignUpTermsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.tita.ui.fragment.signup
+
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tita.R
+
+class SignUpTermsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_certificatio, container, false)
+
+}
