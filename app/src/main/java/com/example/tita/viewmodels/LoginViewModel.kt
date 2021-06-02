fda3e2c0fc66c4b4c8bdcbc1b20801edbde55b20
KyungMin <2004ckck@naver.com>
2021-06-02 17:57:58 +0900

    [UPDATE] BaseViewModel -> AndroidViewModel 로 수정

--- app/src/main/java/com/example/tita/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/tita/viewmodels/LoginViewModel.kt
@@ -1,15 +1,10 @@
 package com.example.tita.viewmodels
 
 import android.app.Application
-import android.content.Context
-import android.content.Intent
-import android.view.View
-import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.AndroidViewModel
 import com.example.tita.base.BaseViewModel
-import com.example.tita.ui.activity.LoginActivity
-import com.example.tita.ui.fragment.SignUptermsFragment
 
-class LoginViewModel(application: Application) : BaseViewModel(application) {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
 
 
