1aab46fc054b743dbc0dd5537dd7ca69efb05dc6
KyungMin <2004ckck@naver.com>
2021-06-02 17:51:28 +0900

    [CREATE] Sign_Up 에 바탕이 되는 Activity 생성

---
+++ app/src/main/java/com/example/tita/ui/activity/SignUpMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tita.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tita.R
+
+class SignUpMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
