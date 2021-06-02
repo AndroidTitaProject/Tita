1a5b8d2bfef1ea98f535833a0cb9e155afa929a8
KyungMin <2004ckck@naver.com>
2021-06-02 19:47:04 +0900

    [CREATE] findFragment

---
+++ app/src/main/java/com/example/tita/IdPasswordFind/FindIdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tita.IdPasswordFind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tita.R
+
+class FindIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_find_id)
+    }
+}
