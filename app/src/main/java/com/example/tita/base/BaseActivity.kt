da371ab00f4d659ae2d585b74d2cc02bd4962547
KyungMin <2004ckck@naver.com>
2021-06-02 18:06:48 +0900

    [CREATE]  DataBinding Base 생성

---
+++ app/src/main/java/com/example/tita/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.example.tita.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VB : ViewDataBinding, VM : BaseViewModel> : AppCompatActivity() {
+
+    lateinit var binding: VB
+
+    abstract val layoutResID: Int
+    abstract val viewModel: VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        performDataBinding()
+    }
+
+    private fun performDataBinding() {
+        binding = DataBindingUtil.setContentView(this, layoutResID)
+
+        binding.lifecycleOwner=this@BaseActivity
+    }
+}
