3b1adf963ad441953dcf57f96dff4f2db9a85419
KyungMin <2004ckck@naver.com>
2021-06-03 16:20:00 +0900

    [ADD] binding, onclick 추가

--- app/src/main/java/com/example/tita/ui/fragment/signup/SignUpTermsFragment.kt
+++ app/src/main/java/com/example/tita/ui/fragment/signup/SignUpTermsFragment.kt
@@ -6,16 +6,31 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tita.R
+import com.example.tita.databinding.FragmentFindIdBinding
+import com.example.tita.databinding.FragmentSignUpTermsBinding
 
 class SignUpTermsFragment : Fragment() {
 
-
+    lateinit var binding: FragmentSignUpTermsBinding
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_certificatio, container, false)
+    ): View {
+
+        val binding: FragmentSignUpTermsBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_sign_up_terms, container, false)
+        binding.fragment = this
+
+        return binding.root
+    }
+
+    fun onclick(v:View){
+        findNavController().navigate(R.id.action_signUpTermsFragment_to_signUpCertificatioFragment)
+    }
+
 
 }
