5f08e5087abc8d98cdcaaa1336a8fb7768cd5b8f
KyungMin <2004ckck@naver.com>
2021-07-15 21:11:48 +0900

    [REPLACE] viewmodel 파일 변경

--- app/src/main/java/com/example/tita/ui/fragment/signup/SignUpIdPasswordFragment.kt
+++ app/src/main/java/com/example/tita/ui/fragment/signup/SignUpIdPasswordFragment.kt
@@ -1,20 +1,13 @@
 package com.example.tita.ui.fragment.signup
 
 import android.graphics.Color
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.tita.R
 import com.example.tita.VIewInterface
 import com.example.tita.base.UtilityBase
 import com.example.tita.databinding.FragmentSignUpIdPasswordBinding
-import com.example.tita.viewmodels.LoginViewModel
 
 class SignUpIdPasswordFragment : UtilityBase.BaseFragment<FragmentSignUpIdPasswordBinding>(R.layout.fragment_sign_up_id_password), VIewInterface {
 
