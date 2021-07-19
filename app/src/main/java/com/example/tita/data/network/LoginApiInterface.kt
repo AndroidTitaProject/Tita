e364c4fc2fa19c97d7541650f171f1c8e79afabe
KyungMin <2004ckck@naver.com>
2021-07-19 19:55:19 +0900

    [UPDATE] @Body > @Filed 어노테이션 변경

--- app/src/main/java/com/example/tita/data/network/LoginApiInterface.kt
+++ app/src/main/java/com/example/tita/data/network/LoginApiInterface.kt
@@ -1,20 +1,25 @@
 package com.example.tita.data.network
 
-import com.example.tita.data.model.Signupdata
+import com.example.tita.data.model.LoginData
+import com.example.tita.data.model.SignUpdData
 import retrofit2.Response
-import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface LoginApiInterface {
 
     @POST("user/signup")
-    fun signup(): Response<Signupdata>
+    fun signup(): Response<SignUpdData>
 
+
+    //Field 형식을 사용할 때는 Form이 Encoding되어야 합니다. 따라서 FormUrlEncoded라는 Annotation을 해주어야 합니다.
+    @FormUrlEncoded
     @POST("user/login")
-    fun postLogin(
-        @Body id: String,
-        @Body pwd: String
-    ): Response<Signupdata>
+    suspend   fun postLogin(
+        @Field("id") id: String,
+        @Field("pwd") pwd: String
+    ): Response<LoginData>
 
     //@GET("/tita/logout")
 }
