00639df7666c00536de602e89f368ccade0060e1
KyungMin <2004ckck@naver.com>
2021-07-20 20:11:32 +0900

    [ADD] fun mail  추가

--- app/src/main/java/com/example/tita/data/network/LoginApiInterface.kt
+++ app/src/main/java/com/example/tita/data/network/LoginApiInterface.kt
@@ -1,11 +1,9 @@
 package com.example.tita.data.network
 
-import com.example.tita.data.model.LoginData
-import com.example.tita.data.model.SignUpdData
+import com.example.tita.data.network.dto.*
 import retrofit2.Response
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
+
 
 interface LoginApiInterface {
 
@@ -16,10 +14,22 @@ interface LoginApiInterface {
     //Field 형식을 사용할 때는 Form이 Encoding되어야 합니다. 따라서 FormUrlEncoded라는 Annotation을 해주어야 합니다.
     @FormUrlEncoded
     @POST("user/login")
-    suspend   fun postLogin(
+    suspend fun postLogin(
         @Field("id") id: String,
         @Field("pwd") pwd: String
     ): Response<LoginData>
 
-    //@GET("/tita/logout")
+    @FormUrlEncoded
+    @POST("user/verify")
+    suspend fun postMail(
+        @Field("mail") mail: String
+    ): Response<MailData>
+
+
+    @POST("user/verify/{key}")
+    fun getPasswordMail(
+        @Path("key") key: String
+    ): Response<MailCertifiedData>
+
+
 }
