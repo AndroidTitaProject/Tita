cdca2ae59007d5f7355a0837b354d5dc3b054b20
KyungMin <2004ckck@naver.com>
2021-09-07 17:57:41 +0900

    [ADD] HiltAndroidApp 어노테이션 추가

--- presentation/src/main/java/com/example/tita/utils/App.kt
+++ presentation/src/main/java/com/example/tita/utils/App.kt
@@ -1,12 +1,14 @@
 package com.example.tita.utils
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 
 /***Application  class
     어플리케이션 컴포넌트들 사이에서 공동으로 멤버들을 사용할 수 있는 편리한 클래스를 제공한다.
 
 */
+@HiltAndroidApp
 class App : Application() {
     companion object {
         lateinit var instance: App
