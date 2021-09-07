394d2809af7b832fa4f1738cad0591cc8a9affe1
KyungMin <2004ckck@naver.com>
2021-09-07 18:16:40 +0900

    [DELETE] onclick 함수 제거

--- presentation/src/main/java/com/example/tita/ui/signup/fragment/SignUpSetNameFragment.kt
+++ presentation/src/main/java/com/example/tita/ui/signup/fragment/SignUpSetNameFragment.kt
@@ -1,15 +1,11 @@
 package com.example.tita.ui.signup.fragment
 
-import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.example.tita.R
-import com.example.tita.VIewInterface
 import com.example.tita.base.UtilityBase
 import com.example.tita.databinding.FragmentSignUpSetNameBinding
 
 class SignUpSetNameFragment :
-    UtilityBase.BaseFragment<FragmentSignUpSetNameBinding>(R.layout.fragment_sign_up_set_name),
-    VIewInterface {
+    UtilityBase.BaseFragment<FragmentSignUpSetNameBinding>(R.layout.fragment_sign_up_set_name) {
 
 
     override fun FragmentSignUpSetNameBinding.onCreateView() {
@@ -19,7 +15,5 @@ class SignUpSetNameFragment :
     override fun FragmentSignUpSetNameBinding.onViewCreated() {
     }
 
-    override fun onclcik(v: View) {
-        findNavController().navigate(R.id.action_signUpSetNameFragment_to_LoginFragment)
-    }
+
 }
