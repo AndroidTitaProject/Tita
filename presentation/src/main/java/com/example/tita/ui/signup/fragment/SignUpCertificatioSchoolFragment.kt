06231da6a296e1cf6a72728b4f8ee232c266f015
KyungMin <2004ckck@naver.com>
2021-09-07 18:15:44 +0900

    [REPLACE] 경로변경

--- presentation/src/main/java/com/example/tita/ui/signup/fragment/SignUpCertificatioSchoolFragment.kt
+++ presentation/src/main/java/com/example/tita/ui/signup/fragment/SignUpCertificatioSchoolFragment.kt
@@ -1,13 +1,10 @@
-package com.example.tita.ui.signup.activity.signup
+package com.example.tita.ui.signup.fragment
 
-import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.example.tita.R
-import com.example.tita.VIewInterface
 import com.example.tita.base.UtilityBase
 import com.example.tita.databinding.FragmentSignUpCertificatioSchoolBinding
 
-class SignUpCertificatioSchoolFragment : UtilityBase.BaseFragment<FragmentSignUpCertificatioSchoolBinding>(R.layout.fragment_sign_up_certificatio_school), VIewInterface {
+class SignUpCertificatioSchoolFragment : UtilityBase.BaseFragment<FragmentSignUpCertificatioSchoolBinding>(R.layout.fragment_sign_up_certificatio_school) {
 
 
     override fun FragmentSignUpCertificatioSchoolBinding.onCreateView() {
@@ -17,7 +14,4 @@ class SignUpCertificatioSchoolFragment : UtilityBase.BaseFragment<FragmentSignUp
     override fun FragmentSignUpCertificatioSchoolBinding.onViewCreated() {
     }
 
-    override fun onclcik(v: View) {
-        findNavController().navigate(R.id.action_signUpCertificatioSchoolFragment_to_signUpSetNameFragment)
-    }
 }
