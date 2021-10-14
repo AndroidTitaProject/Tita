f623e2ccb4e99aaae2f37c390f9d7227b645f2c3
KyungMin <2004ckck@naver.com>
2021-10-14 14:33:40 +0900

    update postSignUp perimeter

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,12 +1,17 @@
 package com.example.domain.repository
 
-import com.example.domain.entity.request.SignUpRequest
-import com.example.domain.entity.response.GetMailEntity
-import com.example.domain.entity.response.SignUpEntity
+import com.example.domain.entity.GetMailEntity
+import com.example.domain.entity.SignUpEntity
 import io.reactivex.rxjava3.core.Single
 
 interface AuthRepository {
-    suspend fun postSignUp(request: SignUpRequest): Single<SignUpEntity>
-    suspend fun getMail(mail: String): Single<GetMailEntity>
+    suspend fun postSignUp(
+         username: String,
+         password: String,
+         name: String,
+         school: String,
+         email: String
+    ): Single<SignUpEntity>
 
+    suspend fun getMail(mail: String): Single<GetMailEntity>
 }
