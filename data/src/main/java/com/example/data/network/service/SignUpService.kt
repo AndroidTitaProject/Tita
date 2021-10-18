0ab1638a086956f097df75bfe1ac7b0fe52a6182
KyungMin <2004ckck@naver.com>
2021-10-18 14:08:53 +0900

    add idCheck, nameOverlap function

--- data/src/main/java/com/example/data/network/service/SignUpService.kt
+++ data/src/main/java/com/example/data/network/service/SignUpService.kt
@@ -1,24 +1,38 @@
 package com.example.data.network.service
 
 import com.example.data.base.BaseResponse
-import com.example.data.entity.GetMailResponse
+import com.example.data.entity.signup.response.GetMailResponse
+import com.example.data.entity.signup.response.IdCheckResponse
+import com.example.data.entity.signup.response.NickNameOverlapResponse
+import com.example.data.entity.signup.request.GetMailRequest
 import com.example.data.entity.signup.request.SignUpRequest
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface SignUpService {
-    @FormUrlEncoded
-    @GET("signup")
-    suspend fun postSignUp(
+    @POST("tita/signup")
+     fun postSignUp(
         @Body request: SignUpRequest
     ): Single<BaseResponse>
 
 
-    @GET("verify/key")
-    suspend fun getMail(
-        @Query("mail") mail: String
+    @POST("tita/verify")
+     fun getMail(
+        @Body request: GetMailRequest
     ): Single<GetMailResponse>
+
+    @GET("tita/username/{username}/exists")
+    fun getIdCheck(
+        @Path("username") userName: String
+    ): Single<IdCheckResponse>
+
+    @GET("tita/username/{name}/exists")
+     fun getNameOverlap(
+        @Path("name") name: String
+    ) : Single<NickNameOverlapResponse>
+
+
 }
