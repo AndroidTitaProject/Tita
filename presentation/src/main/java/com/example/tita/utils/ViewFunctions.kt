da1c66f8a2268254246a86dfe0f54ea90cfd65a4
KyungMin <2004ckck@naver.com>
2021-10-18 13:53:33 +0900

    add setColor success, fall

--- presentation/src/main/java/com/example/tita/utils/ViewFunctions.kt
+++ presentation/src/main/java/com/example/tita/utils/ViewFunctions.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.tita.R
 
 
@@ -11,12 +13,14 @@ fun TextView.errorAnimationShow(context: Context, content: String) {
     text = content
     val animation = AnimationUtils.loadAnimation(context, R.anim.shake_error_text)
     visibility = View.VISIBLE
+    setTextColor(ContextCompat.getColor(context, R.color.error_text))
     startAnimation(animation)
 }
 
 fun TextView.successAnimationShow(context: Context, content: String) {
     text = content
     val animation = AnimationUtils.loadAnimation(context, R.anim.shake_error_text)
+    setTextColor(ContextCompat.getColor(context, R.color.black))
     visibility = View.VISIBLE
     startAnimation(animation)
 }
