172911f1ca76ad9d4d8580287526e04805c8efcc
choiyunsung <12345ggh81@gmail.com>
2021-10-18 21:10:47 +0900

    [DELETE] 필요없는 코드 및 파일삭제

--- data/src/main/java/com/example/data/entity/signup/request/SignUpRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.entity.signup.request
-
-data class SignUpRequest(
-    val username: String,
-    val password: String,
-    val name: String,
-    val school: String,
-    val email: String
-)
