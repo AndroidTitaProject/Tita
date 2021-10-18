a2e702357d2248b212fd01a8bc74e7e141be6d4b
KyungMin <2004ckck@naver.com>
2021-10-18 14:01:28 +0900

    add idCheck, NickName toDomain mapper

--- data/src/main/java/com/example/data/mapper/UserMapper.kt
+++ data/src/main/java/com/example/data/mapper/UserMapper.kt
@@ -1,13 +1,16 @@
 package com.example.data.mapper
 
 import com.example.data.base.BaseResponse
-import com.example.data.entity.GetMailResponse
+import com.example.data.entity.signup.response.GetMailResponse
+import com.example.data.entity.signup.response.IdCheckResponse
+import com.example.data.entity.signup.response.NickNameOverlapResponse
 import com.example.domain.entity.GetMailEntity
+import com.example.domain.entity.IdCheckEntity
+import com.example.domain.entity.NickNameEntity
 import com.example.domain.entity.SignUpEntity
 
 
-
-fun BaseResponse.toDomain() : SignUpEntity{
+fun BaseResponse.toDomain(): SignUpEntity {
     return SignUpEntity(
         this.success,
         this.code,
@@ -16,11 +19,28 @@ fun BaseResponse.toDomain() : SignUpEntity{
 }
 
 
-
-fun GetMailResponse.toDomain() : GetMailEntity {
+fun GetMailResponse.toDomain(): GetMailEntity {
     return GetMailEntity(
         this.success,
         this.code,
         this.msg
+    )
+}
+
+fun IdCheckResponse.toDomain(): IdCheckEntity {
+    return IdCheckEntity(
+        this.success,
+        this.code,
+        this.msg,
+        this.data,
+    )
+}
+
+fun NickNameOverlapResponse.toDomain(): NickNameEntity {
+    return NickNameEntity(
+        this.success,
+        this.code,
+        this.msg,
+        this.data,
     )
 }
