6a2fdaaf010d74169330dbff235d6650f844ea0d
choiyunsung <12345ggh81@gmail.com>
2021-10-21 19:44:00 +0900

    [DELETE] 필요없는 코드 지우기

--- data/src/main/java/com/example/data/entity/findpassword/request/GetMailCodeRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.data.entity.findpassword.request
-
-import com.google.gson.annotations.SerializedName
-
-data class GetMailCodeRequest(
-    @SerializedName("code")val code : String
-)
