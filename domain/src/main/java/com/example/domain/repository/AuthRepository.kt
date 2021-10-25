6caf94601275983704c1ce737afd0b3fb0cc225b
choiyunsung <12345ggh81@gmail.com>
2021-10-25 18:27:09 +0900

    [CREATE] 새로 base 작성 및 비밀번호 찾기 전용 entity 추가

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,23 +1,19 @@
 package com.example.domain.repository
 
-import com.example.domain.entity.GetMailEntity
-import com.example.domain.entity.SignUpEntity
+import com.example.domain.entity.*
 import io.reactivex.rxjava3.core.Single
 
 interface AuthRepository {
     suspend fun postSignUp(
-         username: String,
-         password: String,
-         name: String,
-         school: String,
-         email: String
+        username: String,
+        password: String,
+        name: String,
+        school: String,
+        email: String
     ): Single<SignUpEntity>
 
-    suspend fun postPasswordEmail(email : String, username : String): Single<PostPasswordMailEntity>
-
-    suspend fun getPasswordCode(code : String): Single<GetPasswordCodeEntity>
-
-    suspend fun putChangePassword(username : String,password : String): Single<PutChangePasswordEntity>
-
     suspend fun getMail(mail: String): Single<GetMailEntity>
+
+    suspend fun getIdCheck(userName : String) : Single<IdCheckEntity>
+    suspend fun getNickName(name : String) : Single<NickNameEntity>
 }
