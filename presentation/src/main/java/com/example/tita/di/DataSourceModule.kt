d73a0991faba7b0b04341fe6e27512f72f44517c
KyungMin <2004ckck@naver.com>
2021-10-28 21:31:41 +0900

    [ADD] Provide LoginDataSource

--- presentation/src/main/java/com/example/tita/di/DataSourceModule.kt
+++ presentation/src/main/java/com/example/tita/di/DataSourceModule.kt
@@ -1,5 +1,8 @@
 package com.example.tita.di
 
+
+import com.example.data.network.service.LoginService
+import com.example.data.repository.login.datasource.LoginDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,6 +12,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataSourceModule {
+
+    @Provides
+    @Singleton
+    fun provideLoginDataSource(service: LoginService): LoginDataSourceImpl {
+        return LoginDataSourceImpl(service)
+    }
 
 }
 
