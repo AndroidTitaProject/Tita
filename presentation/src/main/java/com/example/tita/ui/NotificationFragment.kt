dfcf89993f837dd02ce3069c0ed92df01fe0e1cc
ckrudals <2004ckck@naver.com>
2021-12-08 10:17:32 +0900

    add fragment notification

---
+++ presentation/src/main/java/com/example/tita/ui/NotificationFragment.kt
@@ -0,0 +1,8 @@
+package com.example.tita.ui
+
+import com.example.tita.R
+import com.example.tita.base.UtilityBase
+import com.example.tita.databinding.FragmentNotificationBinding
+
+class NotificationFragment : UtilityBase.BaseFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+}
