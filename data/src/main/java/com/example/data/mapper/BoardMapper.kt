9114da7d3d94ff20427e59513956f4cf98185905
choiyunsung <12345ggh81@gmail.com>
2021-12-13 21:10:29 +0900

    [UPDATE] Entity Mapping 추가

--- data/src/main/java/com/example/data/mapper/BoardMapper.kt
+++ data/src/main/java/com/example/data/mapper/BoardMapper.kt
@@ -1,13 +1,22 @@
 package com.example.data.mapper
 
+import com.example.data.entity.board.response.BoardData
 import com.example.data.entity.board.response.BoardResponse
 import com.example.domain.entity.BoardEntity
+import com.google.gson.annotations.SerializedName
 
 fun BoardResponse.toDomain(): BoardEntity {
     return BoardEntity(
         this.code,
-        this.data,
+        this.data.map { it.toEntity() },
         this.msg,
         this.success
+    )
+}
+
+fun BoardData.toEntity() : com.example.domain.entity.BoardData {
+    return com.example.domain.entity.BoardData(
+        this.content,
+        this.postName
     )
 }
