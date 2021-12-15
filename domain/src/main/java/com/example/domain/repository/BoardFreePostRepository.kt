2b2bd16389617ad7781d0ae581ba0d52ef861025
choiyunsung <12345ggh81@gmail.com>
2021-12-15 20:11:25 +0900

    [DELETE] 잘못만든 데이터 삭제

--- domain/src/main/java/com/example/domain/repository/BoardFreePostRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.repository
-
-import com.example.domain.entity.BoardFreePost
-import com.example.domain.entity.BoardFreePostEntity
-import io.reactivex.rxjava3.core.Single
-
-interface BoardFreePostRepository {
-    suspend fun boardFreePost(boardLocation : String,boardFreePostRequest : BoardFreePostEntity) : Single<BoardFreePost>
-}
