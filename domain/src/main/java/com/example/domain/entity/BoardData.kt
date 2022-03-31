35c70ba224c0d3d5dfc868270b57520f10b74c72
choiyunsung <12345ggh81@gmail.com>
2022-03-31 18:16:05 +0900

    ㄷㄱ

--- domain/src/main/java/com/example/domain/entity/BoardData.kt
+++ domain/src/main/java/com/example/domain/entity/BoardData.kt
@@ -3,7 +3,7 @@ package com.example.domain.entity
 import com.google.gson.annotations.SerializedName
 
 data class BoardData(
-    @SerializedName("Content")
+    @SerializedName("Content") 
     val content: String,
     @SerializedName("PostName")
     val postName: String
